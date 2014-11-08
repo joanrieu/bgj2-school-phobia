9dbab723f5c39384cbcb46a1f45460eb75f1e510
Sami DRIDI <sdridi@enseirb-matmeca.fr>
2014-11-08 11:19:55 +0100

    add music playback

--- game.js
+++ game.js
@@ -7,16 +7,45 @@ var g = {
 
 game.state.add('menu', {
     preload: function() {
-        game.load.image('girl', 'assets/girl.png');
+        //game.load.image('title', 'assets/sprites/titlescreen.png');
+        game.load.audio('titleAudio', ['assets/audio/title.mp3','assets/audio/title.ogg'])
+    },
+    create: function() {
+        ///game.physics.startSystem(Phaser.Physics.ARCADE);
+     //   g.title.sprite = game.add.sprite(0, 0, 'title');
+        //game.physics.enable(g.player.sprite, Phaser.Physics.ARCADE);
+        g.keys = {
+            enter: game.input.keyboard.addKey(Phaser.Keyboard.ENTER),
+            spacebar: game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+        };
+        g.music = game.add.audio('titleAudio',1,true);
+        g.music.play('',1,true);
+    },
+    update: function() {
+                if (g.keys.enter.isDown || g.keys.spacebar.isDown) {game.state.start('level1')};
+            },
+    shutdown: function() {
+                g.music.destroy();
+            }
+
+}, true);
+game.state.add('level1',
+    {
+  preload: function() {
+        game.load.image('girl', 'assets/sprites/girl.png');
+        game.load.audio('gameAudio', ['assets/audio/game.mp3','assets/audio/game.ogg']);
+                        console.log("coucou");
     },
     create: function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         g.player.sprite = game.add.sprite(0, 0, 'girl');
         game.physics.enable(g.player.sprite, Phaser.Physics.ARCADE);
         g.cursors = game.input.keyboard.createCursorKeys();
+        music = game.add.audio('gameAudio',1,true);
+        music.play('',1,true);
     },
     update: function() {
         g.player.sprite.body.velocity.x = (g.cursors.right.isDown - g.cursors.left.isDown) * g.player.speed;
         g.player.sprite.body.velocity.y = (g.cursors.down.isDown - g.cursors.up.isDown) * g.player.speed;
     }
-}, true);
+    });
