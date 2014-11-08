5e1ad57436425b39628d14810472bb164bdca976
Sami DRIDI <sdridi@enseirb-matmeca.fr>
2014-11-08 20:47:33 +0100

    add animations for player

--- Game.js
+++ Game.js
@@ -1,13 +1,29 @@
 var game = new Phaser.Game(800, 450);
 var level;
-
-game.state.add('test', {
+var player;
+var input ;
+game.state.add('game', {
     preload: function() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         level = new Level(1);
+        player = new Player;
+        player.preload();
         level.onLevelStart_preload.dispatch();
+        game.load.image('girl_down', 'assets/sprites/girl_down.png');
+        game.load.image('girl_up', 'assets/sprites/girl_up.png');
+        game.load.image('girl_left', 'assets/sprites/girl_left.png');
+        game.load.image('girl_right', 'assets/sprites/girl_right.png');
+        game.load.audio('gameAudio', ['assets/audio/game.mp3', 'assets/audio/game.ogg']);
     },
     create: function() {
         level.onLevelStart_create.dispatch();
+        player.create();
+        input =         {
+            enter: game.input.keyboard.addKey(Phaser.Keyboard.ENTER),
+            spacebar: game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR),
+            cursors: game.input.keyboard.createCursorKeys()
+            //add numbers for inventory?
+        };
     },
     update: function() {
         level.onUpdate.dispatch();
@@ -16,89 +32,80 @@ game.state.add('test', {
 
 // BORDEL BELOW :D -------------------------------------------------------------
 
-var g = {
-    player: {
-        speed: 150
-    }
-};
+// var g = {
+//     player: {
+//         speed: 150
+//     }
+// };
 
-game.state.add('menu', {
-    preload: function() {
-        //game.load.image('title', 'assets/sprites/titlescreen.png');
-        game.load.audio('titleAudio', ['assets/audio/title.mp3', 'assets/audio/title.ogg'])
-    },
-    create: function() {
-        ///game.physics.startSystem(Phaser.Physics.ARCADE);
-        //   g.title.sprite = game.add.sprite(0, 0, 'title');
-        //game.physics.enable(g.player.sprite, Phaser.Physics.ARCADE);
-        g.keys = {
-            enter: game.input.keyboard.addKey(Phaser.Keyboard.ENTER),
-            spacebar: game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
-        };
-        g.music = game.add.audio('titleAudio', 1, true);
-        g.music.play('', 1, true);
-    },
-    update: function() {
-        if (g.keys.enter.isDown || g.keys.spacebar.isDown) {
-            game.state.start('level1');
-        }
-    },
-    shutdown: function() {
-        g.music.destroy();
-    }
+// game.state.add('menu', {
+//     preload: function() {
+//         //game.load.image('title', 'assets/sprites/titlescreen.png');
+//         game.load.audio('titleAudio', ['assets/audio/title.mp3', 'assets/audio/title.ogg'])
+//     },
+//     create: function() {
+//         ///game.physics.startSystem(Phaser.Physics.ARCADE);
+//         //   g.title.sprite = game.add.sprite(0, 0, 'title');
+//         //game.physics.enable(g.player.sprite, Phaser.Physics.ARCADE);
+    
+//         g.music = game.add.audio('titleAudio', 1, true);
+//         g.music.play('', 1, true);
+//     },
+//     update: function() {
+//         }
+//     },
+//     shutdown: function() {
+//         g.music.destroy();
+//     }
 
-});
-game.state.add('level1', {
-    preload: function() {
-        game.load.image('girl_down', 'assets/sprites/girl_down.png');
-        game.load.image('girl_up', 'assets/sprites/girl_up.png');
-        game.load.image('girl_left', 'assets/sprites/girl_left.png');
-        game.load.image('girl_right', 'assets/sprites/girl_right.png');
-        game.load.audio('gameAudio', ['assets/audio/game.mp3', 'assets/audio/game.ogg']);
-    },
-    create: function() {
-        g.player.bmds = [game.make.bitmapData(),
-            game.make.bitmapData(),
-            game.make.bitmapData(),
-            game.make.bitmapData()
-        ]
-        g.player.bmds[0].load('girl_down');
-        g.player.bmds[1].load('girl_up');
-        g.player.bmds[2].load('girl_left');
-        g.player.bmds[3].load('girl_right');
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        g.player.sprite = game.add.sprite(0, 0, g.player.bmds[0]);
-        game.physics.enable(g.player.sprite, Phaser.Physics.ARCADE);
-        g.cursors = game.input.keyboard.createCursorKeys();
-        music = game.add.audio('gameAudio', 1, true);
-        music.play('', 1, true);
+// });
+// game.state.add('level1', {
+//     preload: function() {
 
-        game.stage.smoothed = false;
+//     },
+//     create: function() {
+//         g.player.bmds = [game.make.bitmapData(),
+//             game.make.bitmapData(),
+//             game.make.bitmapData(),
+//             game.make.bitmapData()
+//         ]
+//         g.player.bmds[0].load('girl_down');
+//         g.player.bmds[1].load('girl_up');
+//         g.player.bmds[2].load('girl_left');
+//         g.player.bmds[3].load('girl_right');
+//         game.physics.startSystem(Phaser.Physics.ARCADE);
+//         g.player.sprite = game.add.sprite(0, 0, g.player.bmds[0]);
+//         game.physics.enable(g.player.sprite, Phaser.Physics.ARCADE);
+//         g.cursors = game.input.keyboard.createCursorKeys();
+//         music = game.add.audio('gameAudio', 1, true);
+//         music.play('', 1, true);
 
-        //g.player.bmd.shiftHSL(0, -0.4 , -0.2);
+//         game.stage.smoothed = false;
 
-    },
-    update: function() {
-        var x = g.cursors.right.isDown - g.cursors.left.isDown;
-        var y = g.cursors.down.isDown - g.cursors.up.isDown;
-        if (x && y) {
-            x /= 1.4;
-            y /= 1.4;
-        }
-        g.player.sprite.body.velocity.x = x * g.player.speed;
-        g.player.sprite.body.velocity.y = y * g.player.speed;
-        if (x == -1) {
-            g.player.sprite.loadTexture(g.player.bmds[2]);
-        }
-        if (x == 1) {
-            g.player.sprite.loadTexture(g.player.bmds[3]);
-        }
-        if (y == 1) {
-            g.player.sprite.loadTexture(g.player.bmds[0]);
-        }
-        if (y == -1) {
-            g.player.sprite.loadTexture(g.player.bmds[1]);
-        }
-    }
+//         //g.player.bmd.shiftHSL(0, -0.4 , -0.2);
 
-});
+//     },
+//     update: function() {
+//         var x = g.cursors.right.isDown - g.cursors.left.isDown;
+//         var y = g.cursors.down.isDown - g.cursors.up.isDown;
+//         if (x && y) {
+//             x /= 1.4;
+//             y /= 1.4;
+//         }
+//         g.player.sprite.body.velocity.x = x * g.player.speed;
+//         g.player.sprite.body.velocity.y = y * g.player.speed;
+//         if (x == -1) {
+//             g.player.sprite.loadTexture(g.player.bmds[2]);
+//         }
+//         if (x == 1) {
+//             g.player.sprite.loadTexture(g.player.bmds[3]);
+//         }
+//         if (y == 1) {
+//             g.player.sprite.loadTexture(g.player.bmds[0]);
+//         }
+//         if (y == -1) {
+//             g.player.sprite.loadTexture(g.player.bmds[1]);
+//         }
+//     }
+
+// });
--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 Player = function()
 {
-
+	this.speed = 150;
 	level.onUpdate.add(function()
 	{ 
 		var x = input.cursors.right.isDown - input.cursors.left.isDown;
@@ -12,11 +12,11 @@ Player = function()
 
 		this.cancelMove = false;
 		level.onPrePlayerMove.dispatch()
-		if (x || y){level.onPlayerMove.dispatch(player.sprite.position.x +  x*this.speed,
-												player.sprite.position.y +  y*this.speed);
+		if (x || y){level.onPlayerMove.dispatch(this.sprite.position.x +  x*this.speed,
+												this.sprite.position.y +  y*this.speed);
 		}
 
-		if(input.cursors.spacebar.isDown)
+		if(input.spacebar.isDown)
 		{
 			if(this.target && this.target.interact){this.target.interact()};
 		}
@@ -25,17 +25,20 @@ Player = function()
 		{
 
 
-	        player.sprite.body.velocity.x = x * this.speed;
-	        player.sprite.body.velocity.y = y * this.speed;
+	        this.sprite.body.velocity.x = x * this.speed;
+	        this.sprite.body.velocity.y = y * this.speed;
 
+	        
 	        if (x == -1) {
 	         //load left walk animation
 	        }
 	        if (x == 1) {
 	         //load right walk animation
 	        }
-	        if (y == 1) {
-	         //load down walk animation
+	        if (y == 1 && (this.sprite.animations.currentAnim || {}).name != 'Dwalk') {
+	        	this.sprite.loadTexture('Dwalk');
+	        	this.sprite.animations.add('Dwalk',[0,2]);
+	        	this.sprite.animations.play('Dwalk', 3, true);
 	        }
 	        if (y == -1) {
 	         //load up walk animation
@@ -44,9 +47,14 @@ Player = function()
 
 	},this);
 	this.preload = function() {
-		this.Lwalk = game.load.spritesheet('Lwalk','assets/sprites/girl_left.png',170,300);
-		this.Rwalk = game.load.spritesheet('Lwalk','assets/sprites/girl_right.png',170,300);
-		this.Uwalk = game.load.spritesheet('Lwalk','assets/sprites/girl_up.png',170,300);
-		this.Dwalk = game.load.spritesheet('Lwalk','assets/sprites/girl_down.png',170,300);
+		game.load.spritesheet('Lwalk','assets/sprites/girl_left.png',170,300);
+		game.load.spritesheet('Rwalk','assets/sprites/girl_right.png',170,300);
+		game.load.spritesheet('Uwalk','assets/sprites/girl_up.png',170,300);
+		game.load.spritesheet('Dwalk','assets/sprites/girl_down.png',170,300);
+	};
+	this.create = function() {
+		this.sprite = game.add.sprite(level.state.player.position.x, level.state.player.position.y, 'Dwalk');
+		
+		game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
 	};
 }
